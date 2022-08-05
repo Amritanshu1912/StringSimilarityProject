9a9d30c6064d0765d929d1efee3562099fc34e66
Amritanshu1912 <amritanshusingh3@gmail.com>
2022-08-05 14:05:23 +0530

    base Cases

---
+++ build/CMakeFiles/similarityProjecttest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/similarityProjecttest.dir/similarityProjectTest.cpp.obj"
+  "CMakeFiles/similarityProjecttest.dir/similarityProjectTest.cpp.obj.d"
+  "libsimilarityProjecttest.dll.a"
+  "similarityProjecttest.exe"
+  "similarityProjecttest.exe.manifest"
+  "similarityProjecttest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/similarityProjecttest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/AAAfiles/StringSimilarityProject/googletest/googlemock
+# Build directory: C:/AAAfiles/StringSimilarityProject/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
